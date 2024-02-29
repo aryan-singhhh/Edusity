0ebf462060342a94bf87fdad5f2010488fd6d92d
Aryan Singh <aryansingh007april@gmail.com>
2024-02-29 19:03:30 +0530

    Dynamic Navbar and About Section

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './About.css'
+import about_img from '../../assets/about.png'
+import play_icon from '../../assets/play-icon.png'
+
+
+const About = () => {
+  return (
+    <div className='about'>
+      <div className="about-left">
+        <img src={about_img} alt="" className='about-img'/>
+        <img src={play_icon} alt="" className='play-icon' />
+      </div>
+      <div className="about-right">
+        <h3>About University</h3>
+        <h2>Nurturing Tomorrow's Leaders Today</h2>
+        <p>Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+        <p>All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+
+      </div>
+    </div>
+  )
+}
+
+export default About
